007118b757a33c993dbe456c2294b1f3a1c342d6
marwan <ahmarwan20@gmail.com>
2025-07-22 10:47:32 +0100

    added the variables needed for the code to run

---
+++ modules/dynamodb/variables.tf
@@ -0,0 +1,9 @@
+variable "table_name" {
+  description = "The name of the DynamoDB table."
+  type        = string
+}
+
+variable "project_name" {
+  description = "The name of the overall project."
+  type        = string
+}
